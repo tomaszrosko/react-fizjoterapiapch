06a50e32569ac9fff380361b8e827038a5195737
t.rosko <t.rosko@digibit.pl>
2024-06-02 13:16:29 +0200

    feat(contact) mobile appearance form + add map

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -2,14 +2,26 @@ import "./Contact.scss";
 import React from 'react';
 import Email from "./Email";
 import FooterTop from "../Footer/FooterTop";
+import Map from "./Map"
+import Container from "react-bootstrap/Container";
+import {Row} from "react-bootstrap";
 
 function Contact() {
-  return(
-    <>
-      <Email />
-        <FooterTop />
-    </>
-  )
+    return (
+        <>
+            <section className={"c-contact"}>
+                <Container fluid>
+                    <Container>
+                        <Row>
+                            <Email/>
+                            {/*<Map/>*/}
+                            <FooterTop/>
+                        </Row>
+                    </Container>
+                </Container>
+            </section>
+        </>
+    )
 }
 
 export default Contact;
--- src/components/Contact/Email.js
+++ src/components/Contact/Email.js
@@ -1,66 +1,61 @@
-import React, { useState, useRef } from 'react';
+import React, {useState, useRef} from 'react';
 import emailjs from '@emailjs/browser';
-import Container from "react-bootstrap/Container";
-import { Col, Row } from "react-bootstrap";
+import {Col} from "react-bootstrap";
 import ReCAPTCHA from 'react-google-recaptcha';
 
 const RECAPTCHA_KEY = process.env.React_App_RECAPTCHA_KEY;
 
 const Email = () => {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [message, setMessage] = useState('');
-  const reCAPTCHARef = useRef(null);
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+    const reCAPTCHARef = useRef(null);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+    const handleSubmit = (e) => {
+        e.preventDefault();
 
-    if (!name || !email || !message) {
-      alert('Please fill in all fields.');
-      return;
-    }
+        if (!name || !email || !message) {
+            alert('Please fill in all fields.');
+            return;
+        }
 
-    if (!reCAPTCHARef.current.getValue()) {
-      alert('Please verify that you are not a robot.');
-      return;
-    }
+        if (!reCAPTCHARef.current.getValue()) {
+            alert('Please verify that you are not a robot.');
+            return;
+        }
 
-    // Your EmailJS Data
-    const serviceId = process.env.React_App_SERVICE_ID;
-    const templateId = process.env.React_App_TEMPLATE_ID;
-    const publicKey = process.env.React_App_PUBLIC_KEY;
+        // Your EmailJS Data
+        const serviceId = process.env.React_App_SERVICE_ID;
+        const templateId = process.env.React_App_TEMPLATE_ID;
+        const publicKey = process.env.React_App_PUBLIC_KEY;
 
-    // New Object
-    const templateParams = {
-      from_name: name,
-      from_email: email,
-      to_name: 'Magda',
-      message: message,
+        // New Object
+        const templateParams = {
+            from_name: name,
+            from_email: email,
+            to_name: 'Magda',
+            message: message,
+        };
+
+        // Send Email
+        emailjs.send(serviceId, templateId, templateParams, publicKey)
+            .then((response) => {
+                alert("Email sent successfully!");
+                setName('');
+                setEmail('');
+                setMessage('');
+                reCAPTCHARef.current.reset(); // Reset ReCAPTCHA
+            })
+            .catch((error) => {
+                alert("Error sending email");
+            });
     };
 
-    // Send Email
-    emailjs.send(serviceId, templateId, templateParams, publicKey)
-        .then((response) => {
-          alert("Email sent successfully!");
-          setName('');
-          setEmail('');
-          setMessage('');
-          reCAPTCHARef.current.reset(); // Reset ReCAPTCHA
-        })
-        .catch((error) => {
-          alert("Error sending email");
-        });
-  };
-
-  return (
-      <section className={"c-contact"}>
-        <Container fluid>
-          <Container>
-            <Row className={"c-contact__box"}>
-              <Col xs={12} md={8}>
-                <h2>Kontakt</h2>
-                <form onSubmit={handleSubmit} className='emailForms'>
-                  <label>
+    return (
+        <Col xs={12} md={5} className={"c-contact__box"}>
+            <h2>Kontakt</h2>
+            <form onSubmit={handleSubmit} className='emailForms'>
+                <label>
                     Twoje imię:
                     <input
                         type="text"
@@ -68,8 +63,8 @@ const Email = () => {
                         value={name}
                         onChange={(e) => setName(e.target.value)}
                     />
-                  </label>
-                  <label>
+                </label>
+                <label>
                     Twój Email:
                     <input
                         type="email"
@@ -77,8 +72,8 @@ const Email = () => {
                         value={email}
                         onChange={(e) => setEmail(e.target.value)}
                     />
-                  </label>
-                  <label>
+                </label>
+                <label>
                     Tekst wiadomości:
                     <textarea
                         cols={40}
@@ -87,16 +82,12 @@ const Email = () => {
                         placeholder="Tutaj wpisz treść"
                         onChange={(e) => setMessage(e.target.value)}>
                   </textarea>
-                  </label>
-                  <ReCAPTCHA ref={reCAPTCHARef} sitekey={RECAPTCHA_KEY} />
-                  <button className={"btn-send"} type="submit">Wyślij wiadomość</button>
-                </form>
-              </Col>
-            </Row>
-          </Container>
-        </Container>
-      </section>
-  )
+                </label>
+                <ReCAPTCHA ref={reCAPTCHARef} sitekey={RECAPTCHA_KEY}/>
+                <button className={"btn-send"} type="submit">Wyślij wiadomość</button>
+            </form>
+        </Col>
+    )
 }
 
 export default Email;
